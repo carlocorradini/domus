e35a2c4577c482dd5f28f1f5af3ae82ffca65ced
carlocorradini <carlo.ita98@gmail.com>
2019-03-31 14:09:21 +0200

    minor changes

--- main.c
+++ main.c
@@ -10,6 +10,6 @@ int main() {
     print(COLOR_RED, "Red\n");
     print(COLOR_YELLOW, "Yellow\n");
     print(COLOR_WHITE, "White\n");
-    print_stream(stdout, COLOR_BLUE, "Stampo in stdout con %d parametri: %c | %c", 2, 'A', 'B');
+    print_stream(stdout, COLOR_BLUE, "Stampo in stdout con %d parametri: %c | %c\n", 2, 'A', 'B');
     return EXIT_SUCCESS;
 }
