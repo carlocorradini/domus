2d6759d5d5ce6936caf5cc7fec42fa55e7f675ec
carlocorradini <carlo.ita98@gmail.com>
2019-04-19 17:02:42 +0200

    minor fix

--- project/src/cli/command/command_add.c
+++ project/src/cli/command/command_add.c
@@ -18,7 +18,6 @@ static int _add(char **args) {
         println("\tDevice %s is not supported", args[1]);
     } else {
         if (controller_fork_device(device_descriptor)) {
-            println("%u", getpid());
             println_color(COLOR_GREEN, "\tDevice %s has been added", device_descriptor->name);
             println_color(COLOR_GREEN, "\t\t# %u directly connected device/s", controller_connected_directly());
             println_color(COLOR_GREEN, "\t\t# %u total connected device/s", (controller_connected_total()));
