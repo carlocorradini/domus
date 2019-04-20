13deb202d2b0a80159d7760680598124bbd13063
carlocorradini <carlo.ita98@gmail.com>
2019-04-20 13:32:14 +0200

    Testing PIPES

--- project/src/device/control/device_controller.c
+++ project/src/device/control/device_controller.c
@@ -100,6 +100,7 @@ static bool controller_check_controller(void) {
            controller->device->registry != NULL && controller->device->master_switch != NULL;
 }
 
+/*
 bool controller_fork_device(const DeviceDescriptor *device_descriptor) {
     pid_t pid;
     if (device_descriptor == NULL) return false;
@@ -115,6 +116,71 @@ bool controller_fork_device(const DeviceDescriptor *device_descriptor) {
         return true;
     }
     return false;
+}
+*/
+
+/* /todo PIPE SUPPORT EXAMPLE!*/
+#include <sys/types.h>
+#include <sys/wait.h>
+
+bool controller_fork_device(const DeviceDescriptor *device_descriptor) {
+    int pid, n, c, p, k, nbread;
+    char buf1[12], buf2[12];
+    int fd1[2], fd2[2];
+
+    pipe(fd1);
+    pipe(fd2);
+    pid = fork();
+
+    if (pid == 0) {
+        close(fd1[1]);
+        close(fd2[0]);
+        read(fd1[0], buf2, sizeof(buf2));
+        n = atoi(buf2);
+        printf("Child read %d\n", n);
+        int i;
+        for (i = 0; i < n; i++) {
+            printf("child dozes...\n");
+            sleep(3);
+            printf("child wakes...\n");
+            nbread = read(fd1[0], buf2, sizeof(buf2));
+            if (nbread == -1) {
+                fprintf(stderr, "child exits after read failure\n");
+                exit(1);
+            }
+            c = atoi(buf2);
+            c = c * 2;
+            sprintf(buf2, "%d", c);
+            write(fd2[1], buf2, sizeof(buf2));
+            printf("Child wrote [%s]\n", buf2);
+        }
+        close(fd1[0]);
+        close(fd2[1]);
+        printf("Child done\n");
+        exit(0);
+    } else {
+        close(fd1[0]);
+        close(fd2[1]);
+        printf("Enter integer: ");
+        scanf("%d", &p);
+        sprintf(buf1, "%d", p);
+        write(fd1[1], buf1, sizeof(buf1));
+        printf("Parent wrote [%s]\n", buf1);
+        printf("parent dozes...\n");
+        sleep(3);
+        printf("parent wakes...\n");
+        int i;
+        for (i = 0; i < p; i++) {
+            sprintf(buf1, "%d", i);
+            write(fd1[1], buf1, sizeof(buf1));
+            printf("parent wrote [%s]\n", buf1);
+            read(fd2[0], buf2, sizeof(buf2));
+            printf("number is: %s\n", buf2);
+        }
+        close(fd1[1]);
+        close(fd2[0]);
+        wait(NULL);
+    }
 }
 
 static void controller_fork_child(const DeviceDescriptor *device_descriptor) {
@@ -139,7 +205,7 @@ static void controller_fork_parent(pid_t pid) {
     if (pid < 0) return;
 
     pid_child = (pid_t *) malloc(sizeof(pid_t));
-    if(pid_child == NULL) {
+    if (pid_child == NULL) {
         perror("Controller Fork Parent Memory Allocation");
         exit(EXIT_FAILURE);
     }
